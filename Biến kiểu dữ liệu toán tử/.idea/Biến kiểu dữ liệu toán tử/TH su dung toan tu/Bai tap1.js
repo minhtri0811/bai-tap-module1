61041f642d61e762c6eadb5d67ca2c68ed1314df
unknown <nguyenminhtri08111997@gmail.com>
2021-12-20 11:02:14 +0700

    tao va goi ham

---
+++ Biến kiểu dữ liệu toán tử/.idea/Biến kiểu dữ liệu toán tử/TH su dung toan tu/Bai tap1.js
@@ -0,0 +1,6 @@
+let inputwidth = prompt("nhap chieu dai");
+let inputheight = prompt("nhap chieu cao");
+let width = parseInt(inputwidth);
+let height = parseInt(inputheight);
+let area = width * height;
+document.write('area= ' + area)
